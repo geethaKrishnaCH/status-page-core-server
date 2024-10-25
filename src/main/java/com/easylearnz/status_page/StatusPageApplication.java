912d81c89b901e757fc62941a5b9e06e299279eb
geetha.challa <geetha.challa@004335400582>
2024-10-25 23:10:12 +0530

    Initial Setup

---
+++ src/main/java/com/easylearnz/status_page/StatusPageApplication.java
@@ -0,0 +1,13 @@
+package com.easylearnz.status_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easylearnz/status_page/StatusPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylearnz.status_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
